fcb075966aeb434a9ce44f4209254a7f38085816
chukwu obed <codingwithsmart@gmail.com>
2024-09-08 11:04:47 +0100

    encapsulated the countDown function on the set time

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 "use strick";
 
-const timeWrapper = document.getElementById("time-wrapper"),
-  mainBtn = document.getElementById("timer-btn"),
-  addAlarm = document.getElementById("add-alarm"),
-  timerContainer = document.getElementById("timer-container");
+const timeWrapper = document.getElementById("time-wrapper");
+const timeWrapperArray = document.getElementById("time-wrapper");
+const mainBtn = document.getElementById("timer-btn");
+const addAlarm = document.getElementById("add-alarm");
+const timerContainer = document.getElementById("timer-container");
 
 timeWrapper.classList.add("hide");
 
@@ -67,135 +68,130 @@ addClick();
 
 // Get the time from the input;
 
-const setCountDownTime = () => {
-  const counterWrapper = document.createElement("div");
-  const counterDownTime = document.createElement("div");
+const counterWrapper = document.createElement("div");
+const counterDownTime = document.createElement("div");
 
-  counterWrapper.className = "counterWrapper";
+counterWrapper.className = "counterWrapper";
 
-  let endTime;
+let endTime;
 
-  function getTimes() {
-    const hours = parseInt(document.querySelector(".hour").textContent),
-      minuts = parseInt(document.querySelector(".minus").textContent),
-      seconds = parseInt(document.querySelector(".seconds").textContent);
+function getTimes() {
+  const hours = parseInt(document.querySelector(".hour").textContent),
+    minuts = parseInt(document.querySelector(".minus").textContent),
+    seconds = parseInt(document.querySelector(".seconds").textContent);
 
-    return hours * 60 * 60 * 1000 + minuts * 60 * 1000 + seconds * 1000;
-  }
+  return hours * 60 * 60 * 1000 + minuts * 60 * 1000 + seconds * 1000;
+}
 
-  // format the tiem to count in other
-  function initFutureTime() {
-    const currentTime = new Data().getTime();
-    endTime = currentTime + getTimes();
-  }
+// format the tiem to count in other
 
-  function formatGetTime() {
-    const currentTime = new Date().getTime();
+function formatGetTime() {
+  const currentTime = new Date().getTime();
 
-    const timeDifferent = (endTime - currentTime) / 1000;
+  const timeDifferent = Math.max(Math.round((endTime - currentTime) / 1000));
 
-    console.log(timeDifferent);
+  console.log(timeDifferent);
 
-    const formatHour = Math.floor(timeDifferent / 3600);
-    const formatminutes = Math.floor((timeDifferent % 3600) / 60);
-    const formatSeconds = Math.floor(timeDifferent % 60);
-
-    return {
-      formatHour,
-      formatminutes,
-      formatSeconds,
-      timeDifferent,
-    };
-  }
-
-  function updateTime() {
-    const { formatHour, formatminutes, formatSeconds, timeDifferent } =
-      formatGetTime();
-    const timespans = counterDownTime.querySelectorAll("span");
-    for (const [index, span] of timespans.entries()) {
-      switch (index + 1) {
-        case 1:
-          span.textContent = formatHour.toString().padStart(2, "0");
-          break;
-        case 3:
-          span.textContent = formatminutes.toString().padStart(2, "0");
-          break;
-        case 5:
-          span.textContent = formatSeconds.toString().padStart(2, "0");
-          break;
-        default:
-          break;
-      }
-    }
-    if (timeDifferent < 1) {
-      clearInterval(timeInterval);
-    }
-  }
-
-  // function to store the hour minus and seconds for countDown
-
-  function createTime() {
-    counterWrapper.appendChild(counterDownTime);
-    counterDownTime.className = "counter-container";
-
-    const classNameSpan = [
-      "hourspan",
-      "colun-1",
-      "minutspan",
-      "colun-2",
-      "secondsspan",
-    ];
-
-    for (let className of classNameSpan) {
-      const timeSpan = document.createElement("span");
-      timeSpan.className = className;
-
-      if (className.startsWith("col")) {
-        timeSpan.textContent = ":";
-      }
-      counterDownTime.appendChild(timeSpan);
-    }
-  }
-
-  function createButton() {
-    const buttonContainer = document.createElement("div");
-    counterWrapper.appendChild(buttonContainer);
-
-    buttonContainer.className = "counter-button";
-    const buttons = [
-      '<ion-icon name="trash-outline"></ion-icon>',
-      // '<ion-icon name="pause-outline"></ion-icon>',
-      '<ion-icon name="play-outline"></ion-icon>',
-      '<ion-icon name="refresh-outline"></ion-icon>',
-    ];
-
-    for (let icon of buttons) {
-      const button = document.createElement("button");
-      buttonContainer.appendChild(button);
-
-      button.innerHTML = icon;
-    }
-  }
-
-  let timeInterval;
-
-  function startTime() {
-    createTime();
-    initFutureTime;
-    createButton();
-    updateTime();
-
-    timeInterval = setInterval(updateTime, 1000);
-    timerContainer.appendChild(counterWrapper);
-  }
+  const formatHour = Math.floor(timeDifferent / 3600);
+  const formatminutes = Math.floor((timeDifferent % 3600) / 60);
+  const formatSeconds = Math.floor(timeDifferent % 60);
 
   return {
-    run: startTime
+    formatHour,
+    formatminutes,
+    formatSeconds,
+    timeDifferent,
+  };
+}
+
+function updateTime() {
+  const { formatHour, formatminutes, formatSeconds, timeDifferent } =
+    formatGetTime();
+  const timespans = counterDownTime.querySelectorAll("span");
+  for (const [index, span] of timespans.entries()) {
+    switch (index + 1) {
+      case 1:
+        span.textContent = formatHour.toString().padStart(2, "0");
+        break;
+      case 3:
+        span.textContent = formatminutes.toString().padStart(2, "0");
+        break;
+      case 5:
+        span.textContent = formatSeconds.toString().padStart(2, "0");
+        break;
+      default:
+        break;
+    }
   }
-};
+  if (timeDifferent < 1) {
+    clearInterval(timeInterval);
+  }
+}
+
+// function to store the hour minus and seconds for countDown
+
+function createTime() {
+  counterWrapper.appendChild(counterDownTime);
+  counterDownTime.className = "counter-container";
+
+  const classNameSpan = [
+    "hourspan",
+    "colun-1",
+    "minutspan",
+    "colun-2",
+    "secondsspan",
+  ];
+
+  for (let className of classNameSpan) {
+    const timeSpan = document.createElement("span");
+    timeSpan.className = className;
+
+    if (className.startsWith("col")) {
+      timeSpan.textContent = ":";
+    }
+    counterDownTime.appendChild(timeSpan);
+  }
+}
+
+function createButton() {
+  const buttonContainer = document.createElement("div");
+  counterWrapper.appendChild(buttonContainer);
+
+  buttonContainer.className = "counter-button";
+  const buttons = [
+    '<ion-icon name="trash-outline"></ion-icon>',
+    // '<ion-icon name="pause-outline"></ion-icon>',
+    '<ion-icon name="play-outline"></ion-icon>',
+    '<ion-icon name="refresh-outline"></ion-icon>',
+  ];
+
+  for (let icon of buttons) {
+    const button = document.createElement("button");
+    buttonContainer.appendChild(button);
+
+    button.innerHTML = icon;
+  }
+}
+
+function initFutureTime() {
+  return (endTime = new Date().getTime() + getTimes());
+}
+
+let timeInterval;
+
+function startTime() {
+  createTime();
+  initFutureTime();
+  createButton();
+  updateTime();
+
+  timeInterval = setInterval(updateTime, 1000);
+}
 
 addAlarm.addEventListener("click", () => {
-  setCountDownTime.start();
+  startTime();
+  timerContainer.appendChild(counterWrapper);
+
   timeWrapper.classList.add("hide");
 });
 
